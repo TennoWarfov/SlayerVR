5863c136f903efa79b728dee4b20cefe83a1181c
TennoWarfov <62202736+TennoWarfov@users.noreply.github.com>
2024-09-24 12:51:40 +0500

    loading module frame added

---
+++ Assets/Modules/Loading/Scripts/Loading.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Modules.Loading.Scripts
+{
+    public class Loading : MonoBehaviour
+    {
+        [SerializeField] private Transform playerPosition;
+
+        public void Load()
+        {
+            //TODO: Signal move player;
+            //TODO: Loading logic;
+            //TODO: Signal return player;
+        }
+    }
+}
